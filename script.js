17e9cd3c6856f4feb12c7270430924207aa11637
Swayam <swayam0815@gmail.com>
2023-06-16 18:37:15 -0400

    added more content to homepage

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var descc = document.getElementById('desc');
+
+
 window.addEventListener('load', function() {
     var titleElement = document.getElementById('title');
+    //descc.style.display = 'none';
     titleElement.classList.add('play-animation');
 });
 
@@ -20,47 +24,22 @@ window.onscroll = function() {
 };
 
 
-var divs = document.getElementsByClassName('content');
-var j = 0;
-var container = document.querySelector('.container');
-var divOriginalStyles = [];
+var title = document.getElementById("title-box");
+title.addEventListener('click', function() {
 
-// Store the original styles of each div
-for (var i = 0; i < divs.length; i++) {
-    divOriginalStyles[i] = Object.assign({}, window.getComputedStyle(divs[i]));
-    if (i != 0) {
-        divs[i].style.width = "0px";
-        divs[i].style.height = "0px";
-        divs[i].style.padding = "0px";
-    }
-}
+    title.style.animation = "slide-out 1s forwards";
+    descc.style.display = 'flex';
+
+    descc.style.animation = "slide-up 1s forwards";
+
+    title.style.animation = "slide-down 1s forwards";
+
+})
 
 
-container.addEventListener('click', changeDiv);
+descc.addEventListener('click', function() {
+    title.style.visibility = "visible";
 
-function changeDiv() {
-    divs[j].classList.remove('active');
-    divs[j].style.width = "0px";
-    divs[j].style.height = "0px";
-    divs[j].style.padding = "0px";
+    title.style.animation = "slide-up 1s forwards";
 
-    j = (j + 1) % divs.length;
-
-    setTimeout(function() {
-        divs[j].addEventListener('animationend', handleAnimationEnd);
-        divs[j].classList.add('slide-up');
-
-        divs[j].classList.add('active');
-        divs[j].style.width = divOriginalStyles[j].width;
-        console.log(divs[j].style.width + " and original width is " + divOriginalStyles[j].width);
-        divs[j].style.height = divOriginalStyles[j].height;
-        divs[j].style.padding = divOriginalStyles[j].padding;
-    }, 10);
-}
-
-function handleAnimationEnd(event) {
-    if (event.animationName === 'slide-up') {
-        divs[j].classList.remove('slide-up');
-        divs[j].removeEventListener('animationend', handleAnimationEnd);
-    }
-}
+})
