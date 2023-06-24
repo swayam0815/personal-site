ab83adb06ae4f4a0289301589c4a8b13dd970e50
Swayam <swayam0815@gmail.com>
2023-06-23 22:07:20 -0400

    somewhat homepage animation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var descc = document.getElementById('desc');
-var title = document.getElementById('title-box');
-var about = document.getElementById('about');
-
 var prevScrollPos = window.pageYOffset;
+
+var title = document.getElementById('title');
+var about = document.getElementById('about');
+var over = document.getElementById('overview');
+// over.style.transform = 'translateY(-100vh)';
+
+
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
@@ -17,12 +20,48 @@ window.onscroll = function() {
     prevScrollPos = currentScrollPos;
 };
 
-about.style.visibility = "hidden";
-descc.style.visibility = "hidden";
+var content = document.getElementById('content');
+var child = content.querySelectorAll('div');
+child[0].style.visibility = 'visible';
+child[0].style.opacity = '1';
+for (var i = 0; i < child.length; i++) {
+    console.log(child[i]);
+    // set opacity of all child divs to 0, if it is not the first child
+    if (i != 0) {
+        child[i].style.visibility = 'hidden';
+    }
 
-about.style.animation = "slide-out 1s forwards";
-descc.style.animation = "slide-out2 1s forwards";
-console.log(document.getElementById("index").childElementCount);
-// about.addEventListener('animationend', function() {
-//     about.style.opacity = "0";
+    child[i].addEventListener('click', function(e) {
+        this.style.visibility = 'hidden';
+        this.classList.toggle('slide-down');
+        this.style.opacity = '0';
+        this.nextElementSibling.style.visibility = 'visible';
+        this.nextElementSibling.classList.toggle('slide');
+        this.nextElementSibling.style.opacity = '1';
+        console.log("Hid " + this.id);
+        console.log("Showed " + this.nextElementSibling.id);
+
+    });
+}
+
+// title.addEventListener('click', function() {
+//     title.style.transform = 'translateY(-100vh)';
+//     setTimeout(function() {
+//         title.style.visibility = 'hidden';
+//         title.style.transform = 'translateY(100vh)';
+//         about.style.transform = 'translateY(-100vh)';
+
+//     }, 500);
+
+// })
+
+// about.addEventListener('click', function() {
+//     about.style.transform = 'translateY(-100vh)';
+//     setTimeout(function() {
+//         about.style.visibility = 'hidden';
+//         about.style.transform = 'translateY(100vh)';
+//         over.style.transform = 'translateY(-100vh)';
+
+//     }, 1000);
+
 // })
